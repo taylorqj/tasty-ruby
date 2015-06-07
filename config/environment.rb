6b88633cb15c709721de12ca89d896a0521443af
Taylor Jones <taylorqj@gmail.com>
2015-06-07 00:03:46 -0700

    get dotenv working

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,10 @@
-ENV['RACK_ENV'] ||= 'test'
+require 'dotenv'
+
+# set appropriate values in ENV
+Dotenv.load
+
+ENV.each do |var, value|
+  puts "#{var}=#{value}"
+end
 
 require File.expand_path('../application', __FILE__)
